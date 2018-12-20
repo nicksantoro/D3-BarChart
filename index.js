d5f20b8c6d7cde3fc3f65ee3b8f9f73054d43eeb
Nick Santoro <nicholassantoro@Nicholass-MacBook-Pro.local>
2018-12-20 11:49:47 -0800

    created update function

--- index.js
+++ index.js
@@ -1,83 +1,103 @@
-const svg = d3.select(".canvas")
-  .append("svg")
-  .attr("width", 600)
-  .attr("height", 600);
+// select the svg container first
+const svg = d3.select('.canvas')
+  .append('svg')
+  .attr('width', 600)
+  .attr('height', 600);
 
-// create margins and dimensions
+// create margins & dimensions
 const margin = { top: 20, right: 20, bottom: 100, left: 100 };
 const graphWidth = 600 - margin.left - margin.right;
 const graphHeight = 600 - margin.top - margin.bottom;
 
 const graph = svg.append('g')
-  .attr("width", graphWidth)
-  .attr("height", graphHeight)
-  .attr('transform', `translate(${margin.left},${margin.top})`)
+  .attr('width', graphWidth)
+  .attr('height', graphHeight)
+  .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
-const xAxisGroup = graph.append("g")
-  .attr("transform", `translate(0, ${graphHeight})`)
+// create axes groups
+const xAxisGroup = graph.append('g')
+  .attr('transform', `translate(0, ${graphHeight})`)
 
-const yAxisGroup = graph.append("g");
+xAxisGroup.selectAll('text')
+  .attr('fill', 'orange')
+  .attr('transform', 'rotate(-40)')
+  .attr('text-anchor', 'end');
 
-db.collection("dishes").get().then(res => {
+const yAxisGroup = graph.append('g');
 
-  var data = [];
-  res.docs.forEach(doc => {
-    data.push(doc.data());
-  })
+const y = d3.scaleLinear()
+  .range([graphHeight, 0]);
 
-  console.log(data)
+const x = d3.scaleBand()
+  .range([0, graphWidth])
+  .paddingInner(0.2)
+  .paddingOuter(0.2);
 
-  // find lowest value
-  const min = d3.min(data, d => d.orders);
-  // highest value
-  const max = d3.max(data, d => d.orders);
-  // lowest and highest
-  const extent = d3.extent(data, d => d.orders);
+// create & call axes
+const xAxis = d3.axisBottom(x);
+const yAxis = d3.axisLeft(y)
+  .ticks(3)
+  .tickFormat(d => d + ' orders');
 
-  const y = d3.scaleLinear()
-    .domain([0, max])
-    .range([graphHeight, 0]);
+// the update function
+const update = (data) => {
 
-
-  // band scale
-
-  const x = d3.scaleBand()
-    .domain(data.map(item => item.name))
-    .range([0, 500])
-    .paddingInner(0.2)
-    .paddingOuter(0.2)
-
-  // join the data to rects
+  // join the data to circs
   const rects = graph.selectAll('rect')
-    .data(data)
+    .data(data);
 
-  rects.attr("width", x.bandwidth)
+  // update the domains
+  y.domain([0, d3.max(data, d => d.orders)]);
+  x.domain(data.map(item => item.name));
+
+  // add attrs to rects already in the DOM
+  rects.attr('width', x.bandwidth)
     .attr("height", d => graphHeight - y(d.orders))
-    .attr("fill", "orange")
-    .attr("x", (d) => x(d.name))
-    .attr("y", (d) => y(d.orders))
+    .attr('fill', 'orange')
+    .attr('x', d => x(d.name))
+    .attr('y', d => y(d.orders));
 
+  // append the enter selection to the DOM
   rects.enter()
-    .append("rect")
-    .attr("width", x.bandwidth)
+    .append('rect')
+    .attr('width', x.bandwidth)
     .attr("height", d => graphHeight - y(d.orders))
-    .attr("fill", "red")
-    .attr("x", (d) => x(d.name))
-    .attr("y", (d) => y(d.orders))
-
-  // create and call the axes
-  // pass in x scale
-  const xAxis = d3.axisBottom(x)
-  const yAxis = d3.axisLeft(y)
-    .ticks(3)
-    .tickFormat(d => d + ' orders')
+    .attr('fill', 'orange')
+    .attr('x', (d) => x(d.name))
+    .attr('y', d => y(d.orders));
 
   xAxisGroup.call(xAxis);
   yAxisGroup.call(yAxis);
 
-  xAxisGroup.selectAll("text")
-    .attr("transform", "rotate(-40)")
-    .attr("text-anchor", "end")
-    .attr("fill", "red")
+};
 
-})
+db.collection('dishes').get().then(res => {
+
+  var data = [];
+  res.docs.forEach(doc => {
+    data.push(doc.data());
+  });
+
+  update(data);
+
+});
+// update pattern 5 steps
+
+// const update = (data) => {
+
+//   // update scales (domains) if they rely on our data
+//   y.domain([0, d3.max(data, d => d.orders)]);
+
+//   // join updated data to elements
+//   const rects = graph.selectAll('rect').data(data);
+
+//   // reomove unwanted (if any) shapes using the exit selection
+//   rects.exit().remove();
+
+//   // update current shapes in the dom
+//   rects.attr(...etc);
+
+//   // append the enter selection to the dom
+//   rects.enter().append('rect').attr(...etc);
+
+// };
