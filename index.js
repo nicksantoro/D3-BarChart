db062de2224e83b95c50abb676e5b261ebc329f6
Nick Santoro <nicholassantoro@Nicholass-MacBook-Pro.local>
2018-12-24 12:51:12 -0800

    updating transitions, refactored, added tweens and interpolation

--- index.js
+++ index.js
@@ -39,6 +39,8 @@ const yAxis = d3.axisLeft(y)
   .ticks(3)
   .tickFormat(d => d + ' orders');
 
+const t = d3.transition().duration(1600);
+
 // the update function
 const update = (data) => {
 
@@ -46,24 +48,32 @@ const update = (data) => {
   const rects = graph.selectAll('rect')
     .data(data);
 
+  // remove unwanted rects
+  rects.exit().remove();
+
   // update the domains
   y.domain([0, d3.max(data, d => d.orders)]);
   x.domain(data.map(item => item.name));
 
   // add attrs to rects already in the DOM
   rects.attr('width', x.bandwidth)
-    .attr("height", d => graphHeight - y(d.orders))
     .attr('fill', 'orange')
     .attr('x', d => x(d.name))
+    .transition(t)
+    .attr("height", d => graphHeight - y(d.orders))
     .attr('y', d => y(d.orders));
 
   // append the enter selection to the DOM
   rects.enter()
     .append('rect')
-    .attr('width', x.bandwidth)
-    .attr("height", d => graphHeight - y(d.orders))
+    .attr('width', 0)
+    .attr("height", d => 0)
     .attr('fill', 'orange')
     .attr('x', (d) => x(d.name))
+    .attr('y', d => graphHeight)
+    .transition(t)
+    .attrTween('width', widthTween)
+    .attr("height", d => graphHeight - y(d.orders))
     .attr('y', d => y(d.orders));
 
   xAxisGroup.call(xAxis);
@@ -71,16 +81,47 @@ const update = (data) => {
 
 };
 
-db.collection('dishes').get().then(res => {
+var data = [];
 
-  var data = [];
-  res.docs.forEach(doc => {
-    data.push(doc.data());
+db.collection('dishes').onSnapshot(res => {
+
+  res.docChanges().forEach(change => {
+
+    const doc = { ...change.doc.data(), id: change.doc.id };
+
+    switch (change.type) {
+      case 'added':
+        data.push(doc);
+        break;
+      case 'modified':
+        const index = data.findIndex(item => item.id == doc.id);
+        data[index] = doc;
+        break;
+      case 'removed':
+        data = data.filter(item => item.id !== doc.id);
+        break;
+      default:
+        break;
+    }
+
   });
 
   update(data);
 
 });
+
+// Tweens
+const widthTween = (d) => {
+  let i = d3.interpolate(0, x.bandwidth());
+  return function (t) {
+
+    return i(t);
+  }
+};
+
+
+
+
 // update pattern 5 steps
 
 // const update = (data) => {
